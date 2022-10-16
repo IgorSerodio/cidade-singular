25ab5a7e3a6a9e590ee51878b66100285f849dc9
Pablwo Araujo <pablwo.araujo@ccc.ufcg.edu.br>
2022-10-16 14:27:26 -0300

    updating the project

--- lib/app/screens/map/map_page.dart
+++ lib/app/screens/map/map_page.dart
@@ -11,6 +11,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_flutter_android/google_maps_flutter_android.dart';
+import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({Key? key}) : super(key: key);
